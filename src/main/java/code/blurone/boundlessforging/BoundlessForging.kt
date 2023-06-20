fef64e2dcabca46e7c57f008d9fcf97c629e341c
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-06-20 17:25:16 -0300

    Create project

---
+++ src/main/java/code/blurone/boundlessforging/BoundlessForging.kt
@@ -0,0 +1,13 @@
+package code.blurone.boundlessforging
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BoundlessForging : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
