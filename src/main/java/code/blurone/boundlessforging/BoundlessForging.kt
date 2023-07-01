88d88648f84fd9c284b3d09907277b6066d874b3
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-07-01 00:56:26 -0300

    feat(Boundless)

--- src/main/java/code/blurone/boundlessforging/BoundlessForging.kt
+++ src/main/java/code/blurone/boundlessforging/BoundlessForging.kt
@@ -1,13 +1,28 @@
 package code.blurone.boundlessforging
 
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.PrepareAnvilEvent
 import org.bukkit.plugin.java.JavaPlugin
+import java.awt.List
 
-class BoundlessForging : JavaPlugin() {
+class BoundlessForging : JavaPlugin(), Listener {
     override fun onEnable() {
         // Plugin startup logic
+        server.pluginManager.registerEvents(this, this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun anvilEvent(e: PrepareAnvilEvent)
+    {
+        if (e.inventory.repairCost < e.inventory.maximumRepairCost) return
+
+        e.inventory.maximumRepairCost = Integer.MAX_VALUE
+        e.inventory.contents[2]?.itemMeta?.lore = listOf("§c§lCost: $${e.inventory.repairCost}")
     }
 }
