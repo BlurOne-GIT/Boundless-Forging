b27f15e0d9cf081aeabcb5df680456153aae65ba
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-01-25 18:22:53 -0300

    feat: finish plugin logic

--- src/main/java/code/blurone/boundlessforging/BoundlessForging.kt
+++ src/main/java/code/blurone/boundlessforging/BoundlessForging.kt
@@ -4,24 +4,32 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.PrepareAnvilEvent
+import org.bukkit.inventory.AnvilInventory
 import org.bukkit.plugin.java.JavaPlugin
 
 class BoundlessForging : JavaPlugin(), Listener {
+    private var realMaxRepairCosts: MutableMap<AnvilInventory, Int> = mutableMapOf()
+    private var forceMax = config.getBoolean("forceMaxRepairCost", true)
+
     override fun onEnable() {
         // Plugin startup logic
+        saveDefaultConfig()
         server.pluginManager.registerEvents(this, this)
-    }
-
-    override fun onDisable() {
-        // Plugin shutdown logic
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     fun anvilEvent(e: PrepareAnvilEvent)
     {
-        if (e.inventory.repairCost < e.inventory.maximumRepairCost) return
+        if (e.inventory.maximumRepairCost != Int.MAX_VALUE)
+            realMaxRepairCosts[e.inventory] = e.inventory.maximumRepairCost
 
-        e.inventory.maximumRepairCost = Integer.MAX_VALUE
-        e.inventory.contents[2]?.itemMeta?.lore = listOf("§c§lCost: $${e.inventory.repairCost}")
+        if (e.inventory.repairCost < realMaxRepairCosts[e.inventory]!!)
+        {
+            realMaxRepairCosts.remove(e.inventory)?.let { e.inventory.maximumRepairCost = it }
+            return
+        }
+
+        e.inventory.maximumRepairCost = Int.MAX_VALUE
+        e.inventory.repairCost = if (forceMax) 39 else realMaxRepairCosts[e.inventory]!! - 1
     }
 }
